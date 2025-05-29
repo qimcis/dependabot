38975d9bf2e83c9a3419fac0bf389252e552043f
qimcis <chixie.mcisaac@gmail.com>
2025-05-29 12:56:59 -0400

    changes

---
+++ src/web/__init__.py
@@ -0,0 +1,1 @@
+# This file makes src/web a Python package 
--- src/web/app.py
+++ src/web/app.py
@@ -8,7 +8,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main import (
+from ..main import (
     check_updates_parallel,
     create_github_pr,
     GITHUB_OAUTH_CLIENT_ID,
